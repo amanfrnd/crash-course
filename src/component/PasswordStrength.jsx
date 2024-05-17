01af07d0236bdf69e5a940b857b9fff75d3a93ec
Aman Deep <amandeep.rd@gmail.com>
2024-05-17 19:04:48 +0530

    test1

--- src/component/PasswordStrength.jsx
+++ src/component/PasswordStrength.jsx
@@ -4,7 +4,6 @@ const PasswordStrength = ({password}) => {
   const passwordLength=password.length
 
   const checkStrength = () => {
-    console.log("1")
     if(passwordLength < 1){
       return ''
     }
